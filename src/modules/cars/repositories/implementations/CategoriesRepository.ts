c7258f095f53e53dbdb455168b345985978ff1ec
medriel <adriel.souza397@gmail.com>
2022-05-14 08:17:16 -0300

    Alterando o repositório de categoria

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,16 +1,15 @@
+import { Repository, getRepository } from "typeorm";
 import { Category } from "../../entities/Category";
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
 
-// singleton - Criar uma instancia global para uma classe
-
 class CategoriesRepository implements ICategoriesRepository {
 
-  private categories: Category[];
+  private repository: Repository<Category>;
 
   private static INSTANCE: CategoriesRepository;
 
   private constructor() {
-    this.categories = [];
+    this.repository = getRepository(Category);
   }
 
   public static getInstance(): CategoriesRepository {
@@ -20,24 +19,23 @@ class CategoriesRepository implements ICategoriesRepository {
     return CategoriesRepository.INSTANCE;
   }
 
-  create({ description, name }: ICreateCategoryDTO): void {
-    const category = new Category();
+  async create({ description, name }: ICreateCategoryDTO): Promise<void> {
 
-    Object.assign(category, {
-      name,
+    const category = this.repository.create({
       description,
-      created_at: new Date(),
-    });
+      name,
+    })
 
-    this.categories.push(category);
+    await this.repository.save(category);
   }
 
-  list(): Category[] {
-    return this.categories;
+  async list(): Promise<Category[]> {
+    const categories = await this.repository.find();
+    return categories;
   }
 
-  findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
+  async findByName(name: string): Promise<Category> {
+    const category = await this.repository.findOneBy({ name });
     return category;
   }
 
